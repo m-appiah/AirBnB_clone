2cf88456f34c922a0ec2ae18696c8ffda2b3c049
Abraham Ayamigah <abkc070@gmail.com>
2023-11-13 18:48:22 -0800

    Dealt with import bug

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-from models import storage
+from models.engine.file_storage import FileStorage as storage
 import cmd
 import re
 from shlex import split
